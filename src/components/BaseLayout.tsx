81726c3cf46c952b8b1105a35958bb5aff0d5587
igeligel <kevinigeligeligel@gmail.com>
2024-02-03 17:12:43 +0100

    feat: seo
    Add some basic seo values

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -14,8 +14,27 @@ export const BaseLayout = (props: BaseLayoutProps) => {
   return (
     <>
       <Head>
-        <title>Prisma Starter</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>workplacify</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Box
         as={"main"}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
 import { Box, Container } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
 
 import { CallToActionWithAnnotation } from "../chakra-starter/marketing-ui/call-to-action-with-annotation";
 import { ThreeTierPricing } from "../chakra-starter/marketing-ui/pricing-table.tsx";
 import { SimpleCardWrapper } from "../components/SimpleCardWrapper";
 
 const IndexPage = () => {
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/`;
+  const description =
+    "Optimize your hybrid office schedule with Workplacify - the first open-source, self-hostable desk scheduling platform. Elevate workspace efficiency, manage multiple offices effortlessly, and enhance collaboration with our innovative workplace apps. Start a free trial now and experience the power of desk reservation, floor planning, and advanced workplace analytics. No credit card required.";
   return (
     <>
+      <NextSeo
+        title="workplacify"
+        description={description}
+        canonical={`${url}/`}
+        openGraph={{
+          url,
+          title: "workplacify - Hybrid Office Scheduling Platform",
+          description,
+          images: [
+            {
+              url: "/og-image/home.png",
+              width: 1200,
+              height: 630,
+              alt: "Home picture",
+            },
+          ],
+          site_name: "workplacify",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
       <Container maxW={"container.xl"}>
         <CallToActionWithAnnotation />
       </Container>
